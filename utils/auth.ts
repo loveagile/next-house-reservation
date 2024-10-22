16c8447f2486a483e2e3d1eb6015b79137d2e85a
loveagile <happyplandry@gmail.com>
2024-10-22 09:03:13 +0900

    fix: change child company passwords

--- utils/auth.ts
+++ utils/auth.ts
@@ -4,6 +4,5 @@ export const comparePassword = async (
   password: string,
   hashedPassword: string
 ) => {
-  console.log(await bcryptjs.hash(password, 10));
   return await bcryptjs.compare(password, hashedPassword);
 };
